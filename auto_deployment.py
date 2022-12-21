7684d8cdc7783c78d130cf56ab8d6308df2ff1ba
hahntimo <hahn.timo@yahoo.de>
2022-12-21 22:44:38 +0100

    only pull with active internet connection

--- auto_deployment.py
+++ auto_deployment.py
@@ -4,6 +4,7 @@ import requests
 import time
 from traceback import format_exc
 
+
 class AutoDeployer:
     def __init__(self):
         self.branch = "main"
@@ -11,35 +12,33 @@ class AutoDeployer:
         os.chdir(self.repo_path)
         self.repository = git.Repo(self.repo_path)
 
-    def check_internet_connection(self):
-        for attempt in range(20):
+    def pull(self):
+        for attempt in range(10):
             try:
                 requests.head("https://google.com", timeout=1)
-                return True
+                self.repository.git.checkout(self.branch)
+                current_repo_state = self.repository.head.commit
+                self.repository.remotes.origin.pull()
+
+                if current_repo_state != self.repository.head.commit:
+                    print("NEW VERSION")
+                break
             except:
                 time.sleep(1)
-        return True
 
-    def deploy(self):
-        if self.check_internet_connection():
-            self.repository.git.checkout(self.branch)
-            current_repo_state = self.repository.head.commit
-            self.repository.remotes.origin.pull()
-
-            if current_repo_state != self.repository.head.commit:
-                print("NEW VERSION")
-
-            import main
-            import glob_var
-            glob_var.main_menu_frame = main.MainMenu()
-            glob_var.main_menu_frame.mainloop()
-            exit()
+    def run(self):
+        self.pull()
+        import main
+        import glob_var
+        glob_var.main_menu_frame = main.MainMenu()
+        glob_var.main_menu_frame.mainloop()
+        exit()
 
 try:
     deployer = AutoDeployer()
     with open('test.txt', 'w') as f:
         f.write('success')
-    deployer.deploy()
+    deployer.run()
 
 except:
     with open('test.txt', 'w') as f:
