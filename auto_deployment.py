549ac102e3e2a98cef41d4bc487939cbc1a3dc49
hahntimo <hahn.timo@yahoo.de>
2022-12-21 22:29:57 +0100

    check for internet connection

--- auto_deployment.py
+++ auto_deployment.py
@@ -1,5 +1,6 @@
 import git
 import os
+import requests
 import time
 
 
@@ -9,19 +10,29 @@ class AutoDeployer:
         self.repo_path = os.path.dirname(os.path.abspath(__file__))
         self.repository = git.Repo(self.repo_path)
 
+    def check_internet_connection(self):
+        for attempt in range(20):
+            try:
+                requests.head("https://google.com", timeout=1)
+                return True
+            except:
+                time.sleep(1)
+        return True
+
     def deploy(self):
-        self.repository.git.checkout(self.branch)
-        current_repo_state = self.repository.head.commit
-        self.repository.remotes.origin.pull()
+        if self.check_internet_connection():
+            self.repository.git.checkout(self.branch)
+            current_repo_state = self.repository.head.commit
+            self.repository.remotes.origin.pull()
 
-        if current_repo_state != self.repository.head.commit:
-            print("NEW VERSION")
+            if current_repo_state != self.repository.head.commit:
+                print("NEW VERSION")
 
-        import main
-        import glob_var
-        glob_var.main_menu_frame = main.MainMenu()
-        glob_var.main_menu_frame.mainloop()
-        exit()
+            import main
+            import glob_var
+            glob_var.main_menu_frame = main.MainMenu()
+            glob_var.main_menu_frame.mainloop()
+            exit()
 
 
 deployer = AutoDeployer()
