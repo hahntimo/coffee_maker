e6c91e9b4370452a4e10a7fc197b107a52b701e5
hahntimo <hahn.timo@yahoo.de>
2022-12-23 23:38:50 +0100

    motor test 1

--- controllers/motors.py
+++ controllers/motors.py
@@ -1,4 +1,6 @@
 import threading
+import time
+
 try:
     import RPi.GPIO as GPIO
 except:
@@ -10,8 +12,12 @@ class PitcherSpinnerController:
         self.revolution = 0  # revolutions per minute
         self.direction = 0  # 0 = CW | 1 = CCW
         self.running = False
-        self.DIR_PIN = 20
-        self.STEP_PIN = 16
+        self.delay = 0
+        self.DIR_PIN = 24
+        self.STEP_PIN = 23
+        self.MOTOR_PIN_1 = 14
+        self.MOTOR_PIN_2 = 15
+        self.MOTOR_PIN_3 = 18
 
         self.setup_pins()
 
@@ -19,6 +25,7 @@ class PitcherSpinnerController:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.DIR_PIN, GPIO.OUT)
         GPIO.setup(self.STEP_PIN, GPIO.OUT)
+        GPIO.output((self.MOTOR_PIN_1, self.MOTOR_PIN_2, self.MOTOR_PIN_2), (1, 0, 1))
 
     def start_thread(self):
         threading.Thread(target=self.handler).start()
@@ -28,8 +35,28 @@ class PitcherSpinnerController:
         self.running = True if self.revolution != 0 else False
         self.direction = 0 if new_revolution > 0 else 1
 
+        if new_revolution == 0:
+            self.running = False
+
+        elif new_revolution > 0:
+            self.running = True
+            self.direction = 0
+            GPIO.output(self.DIR_PIN, self.direction)
+            self.delay = 0.0000508
+
+        elif new_revolution < 0:
+            self.running = True
+            self.direction = 1
+            GPIO.output(self.DIR_PIN, self.direction)
+            self.delay = 0.0000508
+
     def handler(self):
         while True:
             while self.running:
                 print(f"subthread: {self.revolution} | {self.direction}")
+                GPIO.output(self.STEP_PIN, GPIO.HIGH)
+                time.sleep(self.delay)
+                GPIO.output(self.STEP_PIN, GPIO.LOW)
+                time.sleep(self.delay)
+
 
